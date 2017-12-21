142d31641feff75ccf38290754c599a4c1242071
Franz Nieschalk <franz.nieschalk@ontos.com>
2017-12-21 14:58:13 +0100

    [Fix] Fixed not working disabled expression.

--- angular-ais-button/ais-button-directive.js
+++ angular-ais-button/ais-button-directive.js
@@ -5,16 +5,14 @@ angular.module('aisButton', ['aisCommon']).directive('aisButton', function() {
 		scope: {
 			label: '@?label',
 			faIcon: '@?faIcon',
-			onClick: '=onClick'
+			onClick: '=onClick',
+			disabled: '&?disabled'
 		},
 		transclude: true,
 		templateUrl: "assets/ai4bd-angular-ais/angular-ais-button/ais-button-view.html",
 		link: function(scope, elem, attrs) {
 			scope.$watch('label', function(value) {
 				scope.ariaLabel = (!!value && value.length > 0) ? value : "Button";
-			});
-			scope.$watch(attrs['ngDisabled'], function(value) {
-				scope.disabled = value;
 			});
 		}
 	}
